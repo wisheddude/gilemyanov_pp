779312a584901da164919384a775f0d3626bd1ee
Wisheddude <wisheddude@gmail.com>
2026-03-09 21:44:04 +0500

    Добавление заполняемых полей и связи между моделями

--- app/Models/Agenda.php
+++ app/Models/Agenda.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Agenda extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'agenda_date',
+        'location',
+        'created_by'
+    ];
+
+    public function creator() {
+        return $this->belongsTo(User::class, 'created_by');
+    }
 }
