5ceb7217514a45657fb64bfaec03d690de862232
Wisheddude <wisheddude@gmail.com>
2026-03-09 21:58:29 +0500

    Добавление тестовой записи в таблицу и обновление логики seeder

--- database/seeders/AgendaSeeder.php
+++ database/seeders/AgendaSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Agenda;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,11 @@ class AgendaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $users = User::all();
+
+        Agenda::factory(20)->make()->each(function ($agenda) use ($users) {
+            $agenda->created_by = $users->random()->id;
+            $agenda->save();
+        });
     }
 }
