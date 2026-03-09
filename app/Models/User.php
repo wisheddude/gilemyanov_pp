9f52162b49c77eb4794b9519537063643e9e5a4a
Wisheddude <wisheddude@gmail.com>
2026-03-09 21:46:26 +0500

    добавление логики обработки роли в модели user

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -44,5 +45,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function agendas() {
+        return $this->hasMany(Agenda::class, 'created_by');
+    }
+
+    public function isAdmin() {
+        return $this->role === 'admin';
     }
 }
