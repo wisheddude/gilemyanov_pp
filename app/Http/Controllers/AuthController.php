b539e52bf40cf7750296b6b1c1fc693bff624a93
Wisheddude <wisheddude@gmail.com>
2026-03-09 22:28:23 +0500

    Добавление логики авторизации

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request) {
+        $credentials = $request->validate([
+           'email' => 'required|email',
+           'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/agendas');
+        }
+
+        return back()->withErrors([
+           'email' => 'Неверный email или пароль'
+        ]);
+    }
+
+    public function logout(Request $request) {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
 }
